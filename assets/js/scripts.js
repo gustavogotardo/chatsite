a64d7343e39d559d1ec9f2455208533561748779
Gustavo Gotardo <gustavo.a.gotardo@gmail.com>
2023-01-16 18:58:46 -0300

    layout update and all users nav bar connection with api

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,17 @@
-function loginDisplay() {
-    
+function getUsers() {
+    const users = axios.get('http://146.190.166.253:4000/users');
+    users.then((res) => {
+        console.log(res.data);
+        document.querySelector(`#allContainer`).innerHTML = ``;
+        for (let i=0; i < res.data.length; i++) {
+            document.querySelector(`#allContainer`).innerHTML += `
+            <li class="user2" onclick="funcao(${res.data[i].id})">
+                <img src="${res.data[i].profilePic}" alt="" class="pfp">
+                <span class="user2Nick">${res.data[i].username}</span>
+            </li>
+            `;
+        }
+    })
 }
+
+getUsers();
